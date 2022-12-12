b24f765999f304769973058f029afb72c8816575
C-SungminLee <91634957+C-SungminLee@users.noreply.github.com>
2022-12-11 22:38:51 -0800

    Update rock.java

--- src/rock.java
+++ src/rock.java
@@ -1,31 +1,42 @@
-import java.util.Scanner;
 public class rock {
-    private String rockName;
-    private int life;
+    private final String rockName;
 
-    public rock(String rockName, int life){
+    /**
+     * rock constructor sets the rockName & life variables in rock.java
+     * @param rockName the pet ROC's name
+     */
+    public rock(String rockName){
         this.rockName = rockName;
-        this.life = life;
     }
-
-    public void rockPaperScissors(){
-        Scanner input = new Scanner(System.in);
-        System.out.println("What do you choose? Rock (R) Paper (P) Scissor (S)?");
-        if ((input.nextLine().toLowerCase()).equals("r")){
+    /**
+     * rockPaperScissors method receives the user's choice in rock paper scissors and returns the result
+     * @param userInput the User's choice in rock paper scissors
+     * @return if the user wins and how much damage the ROC receives
+     */
+    public int rockPaperScissors(String userInput){
+        //if the user chooses rock
+        if (userInput.equals("r")){
             System.out.println("You tied with " + rockName + "!");
-        }else if ((input.nextLine().toLowerCase()).equals("p")){
+            //rock takes 0 damage
+            return 0;
+        //if the user chooses paper
+        }else if (userInput.equals("p")){
             System.out.println("You won against " + rockName + "!");
-            life -=1000;
-
-        }else if((input.nextLine().toLowerCase()).equals("s")){
+            //rock takes 10 damage
+            // return (-10);
+        //if the user chooses scissors
+        }else if(userInput.equals("s")){
             System.out.println("You lost against " + rockName + "!");
-            life +=5;
+            //rock heals 5 damage
+            return (5);
+        //if the user doesn't choose a valid option
         }else{
             System.out.println("Not a valid input. You throw the rock across your room.");
-            life -=2;
+            //rock takes 2 damage
+            return (-2);
         }
 
-
+        return 0;
     }
 }
 
